7a812241e763043d7c0b8c3f2bb8caa104861362
spiritdan <37323553+spiritdan@users.noreply.github.com>
2019-01-18 17:36:46 +0800

    selenium

---
+++ selenium02.py
@@ -0,0 +1,12 @@
+from selenium import webdriver # 调用selenium模块、webdriver模块
+from selenium.webdriver.chrome.options import Options # 调用Option模块
+import time
+
+chrome_options = Options() # 实例化Option对象
+chrome_options.add_argument('--headless') # 把Chrome设置为静默模式
+driver = webdriver.Chrome(options = chrome_options) # 声明浏览器对象
+driver.get('https://localprod.pandateacher.com/python-manuscript/hello-spiderman/') # 访问页面
+time.sleep(2) # 等待2秒
+
+label = driver.find_element_by_tag_name('label') # 解析网页并提取第一个<lable>标签
+print(label.text) # 打印label的文本
--- selenium10.py
+++ selenium10.py
@@ -12,7 +12,7 @@ time.sleep(2)
 button = driver.find_element_by_class_name('js_get_more_hot') # 根据类名找到【点击加载更多】
 button.click() # 点击
 time.sleep(2) # 等待两秒
-
+ 
 pageSource = driver.page_source # 获取Elements中渲染完成的网页源代码
 soup = BeautifulSoup(pageSource,'html.parser') # 使用bs解析网页
 comments = soup.find('ul',class_='js_hot_list').find_all('li',class_='js_cmt_li') # 使用bs提取元素
